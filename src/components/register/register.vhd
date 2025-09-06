669dc6281680ad0c7f72a990ffbfbeea0e9a2de6
lheywang <leonard.heywang@proton.me>
2025-09-06 21:11:01 +0200

    Added first register, tested

--- src/components/register/register.vhd
+++ src/components/register/register.vhd
@@ -2,9 +2,48 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity reg is 
+entity reg is
+    generic (
+        XLEN :      integer := 32
+    );
+    port (
+        -- IO ports
+        datain :    in      std_logic_vector((XLEN - 1) downto 0);      -- Input on the internal bus
+        dataout :   out     std_logic_vector((XLEN -1) downto 0);       -- Output on the internal bud
+
+        -- Clocks
+        clock :     in      std_logic;                                  -- Main clock
+
+        -- Control signals
+        nRST :      in      std_logic;                                  -- Reset. Force a 0'b00--000 value
+        WREN :      in      std_logic;                                  -- Enable the output of the register.
+        INPU :      in      std_logic                                   -- Enable the input of the register. Data will be copied on the next rising edge.         
+    );
 end entity;
 
 architecture behavioral of reg is
+
+        signal data : std_logic_vector((XLEN - 1) downto 0);
+
     begin
+
+        -- Synchronous store
+        P1 : process(clock, nRST, WREN) 
+            begin
+                if (nRST = '0') then
+                    data <= (others => '0');
+
+                elsif rising_edge(clock) then
+                    if (INPU = '1') then
+                        data <= datain;
+                    end if;
+
+                end if;
+
+            end process;
+
+        -- Asyncrhonous writes
+        dataout <= data when (WREN = '1') else
+            (others => 'Z');
+
     end architecture;
---
+++ src/components/register/register_tb.vhd
@@ -0,0 +1,72 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity reg_tb is
+end entity;
+
+architecture behavioral of reg_tb is
+
+    signal datain_t : std_logic_vector(31 downto 0)         := X"deadbeef";
+    signal dataout_t : std_logic_vector(31 downto 0)        := (others => '0');
+    signal clock_t : std_logic                              := '0';
+    signal nRST_t : std_logic                               := '0';
+    signal WREN_t : std_logic                               := '0';
+    signal INPU_t : std_logic                               := '0';
+
+    begin
+
+        U1 : entity work.reg(behavioral)
+            generic map (
+                XLEN => 32
+            )
+            port map (
+                datain => datain_t,
+                dataout => dataout_t,
+                clock => clock_t,
+                nRST => nRST_t,
+                WREN => WREN_t,
+                INPU => INPU_t
+            );
+
+        P1 : process
+            begin
+                clock_t <= not clock_t;
+                wait for 10 ns;
+            end process;
+
+        P2 : process
+            begin
+                wait for 100 ns;
+                nRST_t <= '1';
+            end process;
+
+        P3 : process
+            begin
+                -- strobe to ensure nothing happen while reset
+                wait for 1 ns;
+                INPU_t <= '1';
+                wait for 14 ns;
+                INPU_t <= '0';
+                wait for 5 ns;
+                
+                -- wait for real, and copy the data
+                wait for 100 ns;
+                datain_t <= X"beefdead";
+                INPU_t <= '1';
+                wait for 20 ns;
+                INPU_t <= '0';
+
+                wait for 10 ms;
+            end process;
+
+        P4 : process
+            begin
+                wait for 100 ns;
+                WREN_t <= '1';
+                wait for 40 ns;
+                WREN_t <= '0';
+            end process;
+
+
+    end architecture;
