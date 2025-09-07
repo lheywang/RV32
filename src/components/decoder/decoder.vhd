d001e7e66fb68ca156e5b56484f25ddacecb10b9
lheywang <leonard.heywang@proton.me>
2025-09-07 16:15:13 +0200

    Added a template for the decoder

--- src/components/decoder/decoder.vhd
+++ src/components/decoder/decoder.vhd
@@ -3,8 +3,47 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity decoder is 
+    generic (
+        XLEN :      integer := 32;                                          -- Width of the data outputs. 
+                                                                            -- Warning : This does not change the number of registers not instruction lenght
+        REG_NB :    integer := 32                                           -- Number of processors registers.
+    );
+    port (
+        -- instruction input
+        instruction :   in      std_logic_vector(31 downto 0);
+
+        -- outputs
+        -- buses
+        rs1 :           out     std_logic_vector(REG_NB downto 0);          -- One more register here, for the selection of immediate value.
+        rs2 :           out     std_logic_vector((REG_NB - 1) downto 0);
+        rd :            out     std_logic_vector((REG_NB - 1) downto 0);
+        imm :           out     std_logic_vector((XLEN - 1) downto 0);
+        opcode :        out     std_logic_vector(16 downto 0);              -- ISA use an up to 17 bit opcode.
+        -- signals
+        nILLEGAL :      out     std_logic;
+        counter_en :    out     std_logic;
+
+        -- Clocks
+        clock :         in      std_logic;
+        nRST :          in      std_logic
+    );
 end entity;
 
 architecture behavioral of decoder is
+
+        -- signal
+
     begin
+
+        P1 : process(clock, nRST) 
+            begin
+
+                if (nRST = '0') then
+
+                elsif rising_edge(clock) then
+
+                end if;
+                
+            end process;
+
     end architecture;
--- src/components/decoder/decoder_tb.vhd
+++ src/components/decoder/decoder_tb.vhd
@@ -6,5 +6,42 @@ entity decoder_tb is
 end entity;
 
 architecture behavioral of decoder_tb is
+
+        signal instruction_t :  std_logic_vector(31 downto 0)       := X"00000000";
+        signal rs1_t :          std_logic_vector(32 downto 0);
+        signal rs2_t :          std_logic_vector(31 downto 0);
+        signal rd_t :           std_logic_vector(31 downto 0);
+        signal imm_t :          std_logic_vector(31 downto 0);
+        signal opcode_t :       std_logic_vector(16 downto 0);
+        signal nILLEGAL_t :     std_logic;
+        signal counter_en_t :   std_logic;
+        signal clock_t :        std_logic                           := '0';
+        signal nRST_t :         std_logic                           := '0';
+
     begin
-    end architecture;
+
+        U1 : entity work.decoder(behavioral)
+            generic map (
+                XLEN        => 32,
+                REG_NB      => 32
+            )
+            port map (
+                instruction => instruction_t,
+                rs1         => rs1_t,
+                rs2         => rs2_t,
+                rd          => rd_t,
+                imm         => imm_t,
+                opcode      => opcode_t,
+                nILLEGAL    => nILLEGAL_t,
+                counter_en  => counter_en_t,
+                clock       => clock_t,
+                nRST        => nRST_t
+            );
+
+            P1 : process
+                begin
+                    clock_t <= not clock_t;
+                    wait for 10 ns;
+                end process;
+        
+        end architecture;
