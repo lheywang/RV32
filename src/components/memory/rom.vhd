94ba51f61eec9af1d3aef7c48607aa9400bee2c1
lheywang <leonard.heywang@gmail.com>
2025-09-17 18:39:26 +0200

    Rebuilded ROM, does work a bit better than yesterday, but not enough to synth

--- src/components/memory/rom.vhd
+++ src/components/memory/rom.vhd
@@ -1,280 +1,156 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
--- quartus shall ignore it by itself
-use std.textio.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity rom is 
+entity rom is
     generic (
-        XLEN :          integer := 32;                                                  -- Number of bits stored by the register.
-        ROM_ADDR :      integer := 16#0000_0000#;                                       -- Base address of memory, only used for checking illegal addresses.
-        ROM_SIZE :      integer := 16#0002_FFFF#;                                       -- RAM size in bytes (192 kB by default).
-
-        DEBUG :         boolean := false                                                -- When set to true, call a memory initialization from a file. Otherwise, use the Quartus attribute loaders.
+        XLEN :            integer := 32;
+        ROM_ADDR :        integer := 16#0000_0000#;
+        ROM_SIZE :        integer := 16#0002_FFFF#
     );
     port (
-        -- standard
-        clock :         in  std_logic;                                                  -- clock for the system
-        nRST :          in  std_logic;                                                  -- Reset signal
+        clk         : in  std_logic;
+        nRST        : in  std_logic;
 
-        -- Bus IOs
-        addr1 :         in  std_logic_vector((XLEN - 1) downto 0);                      -- address bus 1
-        addr2 :         in  std_logic_vector((XLEN - 1) downto 0);                      -- address bus 2
-        dataout1 :      out std_logic_vector((XLEN - 1) downto 0)   := (others => '0'); -- data output 1
-        dataout2 :      out std_logic_vector((XLEN - 1) downto 0)   := (others => '0'); -- data output 2
-        byte_en1 :      in  std_logic_vector(((XLEN / 8) - 1) downto 0);                -- byte enabling 1
-        byte_en2 :      in  std_logic_vector(((XLEN / 8) - 1) downto 0);                -- byte enabling 2
-        data_valid1 :   out std_logic;                                                  -- Enable the data is valid for usage 1
-        data_valid2 :   out std_logic;                                                  -- Enable the data is valid for usage 2
+        -- Port 0: Instruction fetch
+        addr0       : in  std_logic_vector((XLEN - 1) downto 0);
+        dataout0    : out std_logic_vector((XLEN - 1) downto 0);
+        mem_req0    : in  std_logic;
+        mem_busy0   : out std_logic;
+        data_valid0 : out std_logic;
 
-        -- status
-        busy1 :         out std_logic                               := '0';             -- Indicate that a memory operation is currently running 1
-        busy2 :         out std_logic                               := '0';             -- Indicate that a memory operation is currently running 2
-        illegal_addr :  out std_logic                               := '0'              -- Indicate an impossible memory address (on any of the ports)
+        -- Port 1: General-purpose
+        addr1       : in  std_logic_vector((XLEN - 1) downto 0);
+        byte_en1    : in  std_logic_vector(3 downto 0);
+        dataout1    : out std_logic_vector((XLEN - 1) downto 0);
+        mem_req1    : in  std_logic;
+        mem_busy1   : out std_logic;
+        data_valid1 : out std_logic
     );
-end entity;
+end rom;
 
 architecture behavioral of rom is
 
-        -- Create the memory array
-        type rom_array is array(0 to (ROM_SIZE / 4)) of std_logic_vector((XLEN - 1) downto 0);
-        
+    type rom_array is  array(0 to (ROM_SIZE / 4)) of std_logic_vector(XLEN-1 downto 0);
+    
 
-        -- Internal busy signal
-        signal mem_busy1 : std_logic := '0';
-        signal mem_busy2 : std_logic := '0';
-        signal wrong_addr1 : std_logic := 'X';
-        signal wrong_addr2 : std_logic := 'X';
+    -- internal registers for one-cycle latency
+    signal dout0_reg, dout1_reg : std_logic_vector(XLEN-1 downto 0);
+    signal busy0_reg, busy1_reg : std_logic;
+    signal valid0_reg, valid1_reg : std_logic;
 
-        -- function hex_char_to_int(c : in character) return integer is
-        -- begin
-        --     if c >= '0' and c <= '9' then
-        --         return character'pos(c) - character'pos('0');
-        --     elsif c >= 'A' and c <= 'F' then
-        --         return 10 + character'pos(c) - character'pos('A');
-        --     elsif c >= 'a' and c <= 'f' then
-        --         return 10 + character'pos(c) - character'pos('a');
-        --     else
-        --         return 0;
-        --     end if;
-        -- end function;
-
-        -- function hex_to_slv(hex : in string; bits : in natural) return std_logic_vector is
-        --     variable result : std_logic_vector(bits-1 downto 0) := (others => '0');
-        --     variable val    : integer := 0;
-        -- begin
-        --     for i in hex'range loop
-        --         val := val * 16 + hex_char_to_int(hex(i));
-        --     end loop;
-        --     result := std_logic_vector(to_unsigned(val, bits));
-        --     return result;
-        -- end function;
-
-        -- procedure load_mem_from_file(signal outmem : out rom_array; fname : in string) is
-        --     file f      : text open read_mode is fname;
-        --     variable l  : line;
-        --     variable s  : string(1 to 8);  -- 8 hex chars per line
-        --     variable v  : std_logic_vector(31 downto 0);
-        --     variable idx : integer := 0;
-        --     variable i   : integer;
-        --     variable len : integer;
-        -- begin
-        --     while not endfile(f) loop
-        --         readline(f, l);  -- read one line
-
-        --         -- Fill s safely, pad if line too short
-        --         len := l'length;
-        --         for i in 1 to 8 loop
-        --             if i <= len then
-        --                 s(i) := l(i);
-        --             else
-        --                 s(i) := '0';
-        --             end if;
-        --         end loop;
-
-        --         -- Convert hex string to std_logic_vector
-        --         v := hex_to_slv(s, 32);
-        --         outmem(idx) <= v;
-        --         idx := idx + 1;
-        --     end loop;
-        -- end procedure;
-
-        -- For quartus build only : Initialyze ROM from file
-        -- attribute ram_init_file : string;
-        -- attribute ram_init_file of mem : signal is "code/output/rom.hex";
-
-        impure function init_ram_hex return rom_array is
-            file text_file : text open read_mode is "code/output/rom.bin";
-            variable text_line : line;
-            variable ram_content : rom_array;
-            variable c : character;
-            variable offset : integer;
-            variable hex_val : std_logic_vector(3 downto 0);
-        begin
+    impure function init_ram_hex return rom_array is
+        variable ram_content : rom_array;
+        variable last : integer;
+    begin
+        last := 0;
         for i in 0 to rom_array'high - 1 loop
-            readline(text_file, text_line);
-        
-            offset := 0;
-        
-            while offset < ram_content(i)'high loop
-                read(text_line, c);
-            
-                case c is
-                    when '0' => hex_val := "0000";
-                    when '1' => hex_val := "0001";
-                    when '2' => hex_val := "0010";
-                    when '3' => hex_val := "0011";
-                    when '4' => hex_val := "0100";
-                    when '5' => hex_val := "0101";
-                    when '6' => hex_val := "0110";
-                    when '7' => hex_val := "0111";
-                    when '8' => hex_val := "1000";
-                    when '9' => hex_val := "1001";
-                    when 'A' | 'a' => hex_val := "1010";
-                    when 'B' | 'b' => hex_val := "1011";
-                    when 'C' | 'c' => hex_val := "1100";
-                    when 'D' | 'd' => hex_val := "1101";
-                    when 'E' | 'e' => hex_val := "1110";
-                    when 'F' | 'f' => hex_val := "1111";
-            
-                    when others =>
-                    hex_val := "XXXX";
-                    assert false report "Found non-hex character '" & c & "'";
-                end case;
-            
-                ram_content(i)(ram_content(i)'high - offset
-                    downto ram_content(i)'high - offset - 3) := hex_val;
-                offset := offset + 4;
-            
-                end loop;
-            end loop;
-  
-            return ram_content;
-        end function;
+            if last = 0 then
+                ram_content(i) := "10101010101010101010101010101010";
+                last := 1;
+            else
+                ram_content(i) := "01010101010101010101010101010101";
+                last := 0;
+            end if;
+         end loop;
 
-        signal mem : rom_array := init_ram_hex;
+        return ram_content;
+    end function;
+
+    signal mem : rom_array := init_ram_hex;  -- init later with MIF/HEX
+
+begin
+
+    process(clk, nRST)
+
+        variable word_addr0 : integer;
+        variable dout_var0 : std_logic_vector(XLEN-1 downto 0);
 
     begin
+        if nRST = '0' then
+            dataout0 <= (others => '0');
+            busy0_reg <= '0';
+            data_valid0 <= '0';
 
-        -- For GHDL only : Initialize ROM from file
-        -- ghdl_loader : if DEBUG generate
-        --     P0 : process(nRST)
-        --     begin
-        --         if (nRST = '0') then
-        --             load_mem_from_file(mem, );
-        --         end if;
-        --     end process;
-        -- end generate;
-         
-        P10 : process(clock, nRST)
+        elsif rising_edge(clk) then
+            
+            -- === Port 0: Instruction fetch ===
+            if mem_req0 = '1' and busy0_reg = '0' then
 
-            variable word_addr1 : integer;
+                word_addr0 := to_integer(unsigned(addr0)) / 4;
+                dataout0 <= mem(word_addr0);
+                data_valid0 <= '0';
 
-        begin
-            if (nRST = '0') then
-                mem_busy1 <= '0';
-                dataout1 <= (others => '0');
-                busy1 <= '0';
-                data_valid1 <= '0';
+            elsif busy0_reg = '1' then
 
-            elsif rising_edge(clock) then
+                busy0_reg <= '0';
+                data_valid0 <= '1';
 
-                if (mem_busy1 = '0') then -- Memory is busy, don't look for memory IO. The signal is also sent to controller.
-                    if (wrong_addr1 = '0') then
+            elsif mem_req0 = '0' then
 
-                        -- assign the word address value
-                        word_addr1 := to_integer((unsigned(addr1) - ROM_ADDR) / 4);
+                dataout0 <= (others => 'Z');
 
-
-                        for i in 0 to 3 loop
-                            if byte_en1(i) = '1' then
-                                dataout1(8*(i+1)-1 downto 8*i) <= mem(word_addr1)(8*(i+1)-1 downto 8*i);
-                            end if;
-                        end loop;
-
-                        mem_busy1 <= '1';
-
-                    else
-                        dataout1 <= (others => '0');
-
-                    end if;
-
-                    data_valid1 <= '0';
-
-                else -- Memory IO take a single cycle on Intel M9K, so we can safely reset it here.
-                    mem_busy1 <= '0';
-                    data_valid1 <= '1';
-
-                end if;
-
-            end if;
-
-        end process;
-
-        P11 : process(clock, nRST)
-
-            variable word_addr2 : integer;
-
-        begin
-            if (nRST = '0') then
-                mem_busy2 <= '0';
-                dataout2 <= (others => '0');
-                busy2 <= '0';
-                data_valid2 <= '0';
-
-            elsif rising_edge(clock) then
-
-                if (mem_busy2 = '0') then -- Memory is busy, don't look for memory IO. The signal is also sent to controller.
-                    if (wrong_addr2 = '0') then
-
-                        -- assign the word address value
-                        word_addr2 := to_integer((unsigned(addr2) - ROM_ADDR) / 4);
-
-
-                        for i in 0 to 3 loop
-                            if byte_en2(i) = '1' then
-                                dataout2(8*(i+1)-1 downto 8*i) <= mem(word_addr2)(8*(i+1)-1 downto 8*i);
-                            end if;
-                        end loop;
-
-                        mem_busy2 <= '1';
-
-                    else
-                        dataout2 <= (others => '0');
-
-                    end if;
-
-                    data_valid2 <= '0';
-
-                else -- Memory IO take a single cycle on Intel M9K, so we can safely reset it here.
-                    mem_busy2 <= '0';
-                    data_valid2 <= '1';
-
-                end if;
-
-            end if;
-
-        end process;
-
-        -- Check for any changes of address if it is valid
-        P2 : process(addr1, addr2)
-        begin
-            if ((unsigned(addr1) + 1 > (ROM_ADDR)) and ((unsigned(addr1) < (ROM_ADDR + ROM_SIZE + 1)))) then
-                wrong_addr1 <= '0';
             else
-                wrong_addr1 <= '1';
+                
+                data_valid0 <= '0';
+
             end if;
 
-            if ((unsigned(addr2) + 1 > (ROM_ADDR)) and ((unsigned(addr2) < (ROM_ADDR + ROM_SIZE + 1)))) then
-                wrong_addr2 <= '0';
-            else
-                wrong_addr2 <= '1';
-            end if;
+        end if;
 
-        end process;
-    
-        illegal_addr <= wrong_addr1 or wrong_addr2;
-        busy1 <= mem_busy1;
-        busy2 <= mem_busy2;
+    end process;
 
-    end architecture;
+    --     process(clk, nRST)
+
+    --     variable word_addr1 : integer;
+    --     variable dout_var1 : std_logic_vector(XLEN-1 downto 0);
+
+    -- begin
+    --     if nRST = '0' then
+    --         dataout1 <= (others => '0');
+    --         busy1_reg <= '0';
+    --         data_valid1 <= '0';
+
+    --     elsif rising_edge(clk) then
+            
+    --         -- === Port 1: Data access ===
+    --         if mem_req1 = '1' and busy1_reg = '0' then
+
+    --             word_addr1 := to_integer(unsigned(addr1)) / 4;
+    --             dout_var1 := (others => '0');  -- default for byte_en
+
+    --             for i in 0 to byte_en1'high loop
+    --                 if byte_en1(i) = '1' then
+    --                     dout_var1(8*(i+1)-1 downto 8*i) := mem(word_addr1)(8*(i+1)-1 downto 8*i);
+    --                 end if;
+    --             end loop;
+
+    --             dataout1 <= dout_var1;
+    --             busy1_reg <= '1';
+    --             data_valid1 <= '0';
+
+    --         elsif busy1_reg = '1' then
+
+    --             busy1_reg <= '0';
+    --             data_valid1 <= '1';
+
+    --         elsif mem_req1 = '0' then
+
+    --             dataout1 <= (others => 'Z');
+
+    --         else
+
+    --             data_valid1 <= '0';
+
+    --         end if;
+
+    --     end if;
+
+    -- end process;
+
+    -- Assign outputs
+    mem_busy0   <= busy0_reg;
+    mem_busy1   <= busy1_reg;
+
+
+end behavioral;
--- src/components/memory/rom_tb.vhd
+++ src/components/memory/rom_tb.vhd
@@ -12,15 +12,16 @@ architecture behavioral of rom_tb is
         signal nRST_t :         std_logic                       := '0';
         signal addr1_t :        std_logic_vector(31 downto 0)   := (others => '0');
         signal addr2_t :        std_logic_vector(31 downto 0)   := (others => '0');
-        signal dataout1_t :     std_logic_vector(31 downto 0)   := (others => '0');
-        signal dataout2_t :     std_logic_vector(31 downto 0)   := (others => '0');
+        signal dataout1_t :     std_logic_vector(31 downto 0)   ;
+        signal dataout2_t :     std_logic_vector(31 downto 0)   ;
         signal busy1_t :        std_logic                       := '0';
         signal busy2_t :        std_logic                       := '0';
         signal illegal_addr_t : std_logic                       := '0';
         signal byte_en1_t :     std_logic_vector(3 downto 0)    := "0011";
-        signal byte_en2_t :     std_logic_vector(3 downto 0)    := "1100";
         signal data_valid1_t :  std_logic                       := '0';
         signal data_valid2_t :  std_logic                       := '0';
+        signal mem_req1_t :     std_logic                       := '0';
+        signal mem_req2_t :     std_logic                       := '0';
 
     begin
 
@@ -28,23 +29,22 @@ architecture behavioral of rom_tb is
         generic map (
             XLEN            => 32,
             ROM_ADDR        => 16#0000_0000#,
-            ROM_SIZE        => 16#0002_FFFF#,
-            DEBUG           => true
+            ROM_SIZE        => 16#0002_FFFF#
         )
         port map (
-            clock           => clock_t,
+            clk             => clock_t,
             nRST            => nRST_t,
-            addr1           => addr1_t,
-            addr2           => addr2_t,
+            addr0           => addr1_t,
+            addr1           => addr2_t,
+            dataout0        => dataout1_t,
             dataout1        => dataout1_t,
-            dataout2        => dataout1_t,
-            busy1           => busy1_t,
-            busy2           => busy2_t,
-            illegal_addr    => illegal_addr_t,
+            mem_busy0       => busy1_t,
+            mem_busy1       => busy2_t,
             byte_en1        => byte_en1_t,
-            byte_en2        => byte_en2_t,
-            data_valid1     => data_valid1_t,
-            data_valid2     => data_valid2_t
+            data_valid0     => data_valid1_t,
+            data_valid1     => data_valid2_t,
+            mem_req0        => mem_req1_t,
+            mem_req1        => mem_req2_t
         );
 
         -- clock
@@ -92,4 +92,20 @@ architecture behavioral of rom_tb is
             addr2_t <= X"0000_bbbb";
             wait for 40 ns;
         end process;
+
+        P6 : process
+        begin
+            mem_req1_t <= '1';
+            wait for 40 ns;
+            mem_req2_t <= '1';
+            wait for 40 ns;
+            mem_req2_t <= '0';
+            wait for 40 ns;
+            mem_req2_t <= '1';
+            wait for 40 ns;
+            mem_req2_t <= '0';
+            mem_req1_t <= '0';
+            wait for 40 ns;
+        end process;
+
     end architecture;
