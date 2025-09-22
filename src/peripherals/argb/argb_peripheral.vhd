669062253b1ffba4c44ea0cbed48516128dd151c
lheywang <leonard.heywang@gmail.com>
2025-09-22 19:42:18 +0200

    Moved elements to represent in a better way the impl.

---
+++ src/peripherals/argb/argb_peripheral.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity argb_peripheral is 
+end entity;
+
+architecture behavioral of argb_peripheral is
+    begin
+    end architecture;
---
+++ src/peripherals/argb/argb_peripheral_tb.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity argb_peripheral_tb is 
+end entity;
+
+architecture behavioral of argb_peripheral_tb is
+    begin
+    end architecture;
