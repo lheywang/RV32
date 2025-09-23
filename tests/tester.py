11d1ed70cd610c6010c556ade85ef6159d6fcf88
lheywang <leonard.heywang@gmail.com>
2025-09-23 18:26:15 +0200

    Updated comments

--- tests/tester.py
+++ tests/tester.py
@@ -1,11 +1,11 @@
-# ============================================================
-#                       tester.py
+# ================================================================================
+#                                   tester.py
 #
-#                       l.heywang
-#                       23/09/2025
+#                                   l.heywang
+#                                   23/09/2025
 #
-# Assemble assembly files, simulate HDL core with the program
-# in memory, and read-back some signals to ensure everything
-# is working nicely.
-# Iterate over multiples folders to tests differents cases.
-# ============================================================
+#       Assemble assembly files, simulate HDL core with the program
+#       in memory, and read-back some signals to ensure everything
+#       is working nicely.
+#       Iterate over multiples folders to tests differents cases.
+# =================================================================================
