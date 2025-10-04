fbe7a3c1983b1ed569a58b858d935ab99f3760f1
lheywang <leonard.heywang@proton.me>
2025-10-04 15:42:57 +0200

    Formatted files

--- src/core/csr/csr_registers.vhd
+++ src/core/csr/csr_registers.vhd
@@ -1,139 +1,137 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.common.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.common.ALL;
 
-entity csr_registers is
-    generic (
-        XLEN    : integer := 32
+ENTITY csr_registers IS
+    GENERIC (
+        XLEN : INTEGER := 32
     );
-    port (
-        clock   : in  std_logic;
-        clock_en : in std_logic;
-        nRST    : in  std_logic;
+    PORT (
+        clock : IN STD_LOGIC;
+        clock_en : IN STD_LOGIC;
+        nRST : IN STD_LOGIC;
 
         -- single write port
-        we      : in  std_logic;
-        wa      : in  csr_register;
-        wd      : in  std_logic_vector((XLEN - 1) downto 0);
+        we : IN STD_LOGIC;
+        wa : IN csr_register;
+        wd : IN STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
 
         -- two read ports
-        ra1     : in  csr_register;
-        rd1     : out std_logic_vector((XLEN - 1) downto 0);
+        ra1 : IN csr_register;
+        rd1 : OUT STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
 
         -- Interrupt specific IOs
-        int_vec : in  std_logic_vector((XLEN - 1) downto 0);
-        int_out : out std_logic;
-        int_en :  out std_logic
+        int_vec : IN STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+        int_out : OUT STD_LOGIC;
+        int_en : OUT STD_LOGIC
     );
-end entity;
+END ENTITY;
 
-architecture rtl of csr_registers is
+ARCHITECTURE rtl OF csr_registers IS
 
     -- Define write permissions for each register
-    constant MSTATUS_W_MASK :   std_logic_vector((XLEN - 1) downto 0) := "00000000000000000111100010001000";
-    constant MISA_W_MASK :      std_logic_vector((XLEN - 1) downto 0) := "00000000000000000000000000000000";
-    constant MIE_W_MASK :       std_logic_vector((XLEN - 1) downto 0) := "11111111111111110000100010001000";
-    constant MTVEC_W_MASK :     std_logic_vector((XLEN - 1) downto 0) := "11111111111111111111111100000001";
-    constant MSCRATCH_W_MASK :  std_logic_vector((XLEN - 1) downto 0) := "11111111111111111111111111111111";
-    constant MEPC_W_MASK :      std_logic_vector((XLEN - 1) downto 0) := "11111111111111111111111111111110";
-    constant MCAUSE_W_MASK :    std_logic_vector((XLEN - 1) downto 0) := "10000000000000000000000000011111";
-    constant MTVAL_W_MASK :     std_logic_vector((XLEN - 1) downto 0) := "00000000000000000000000000000000";
-    constant MIP_W_MASK :       std_logic_vector((XLEN - 1) downto 0) := "00000000000000000000000000000000";    
+    CONSTANT MSTATUS_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "00000000000000000111100010001000";
+    CONSTANT MISA_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "00000000000000000000000000000000";
+    CONSTANT MIE_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "11111111111111110000100010001000";
+    CONSTANT MTVEC_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "11111111111111111111111100000001";
+    CONSTANT MSCRATCH_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "11111111111111111111111111111111";
+    CONSTANT MEPC_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "11111111111111111111111111111110";
+    CONSTANT MCAUSE_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "10000000000000000000000000011111";
+    CONSTANT MTVAL_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "00000000000000000000000000000000";
+    CONSTANT MIP_W_MASK : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0) := "00000000000000000000000000000000";
 
     -- Since there's not a lot of registers, we define them manually.
-    signal mstatus :            std_logic_vector((XLEN - 1) downto 0);
-    signal misa :               std_logic_vector((XLEN - 1) downto 0);
-    signal mie :                std_logic_vector((XLEN - 1) downto 0);
-    signal mtvec :              std_logic_vector((XLEN - 1) downto 0);
-    signal mscratch :           std_logic_vector((XLEN - 1) downto 0);
-    signal mepc :               std_logic_vector((XLEN - 1) downto 0);
-    signal mcause :             std_logic_vector((XLEN - 1) downto 0);
-    signal mtval :              std_logic_vector((XLEN - 1) downto 0);
-    signal mip :                std_logic_vector((XLEN - 1) downto 0);
+    SIGNAL mstatus : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL misa : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mie : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mtvec : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mscratch : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mepc : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mcause : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mtval : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+    SIGNAL mip : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
 
     -- Defining logic mask function for easier updates
-    function update_bits(
-        write_in :      std_logic_vector((XLEN - 1) downto 0);
-        write_mask :    std_logic_vector((XLEN - 1) downto 0);
-        write_old :     std_logic_vector((XLEN - 1) downto 0))
-        return std_logic_vector is
-        begin
-            return (write_in and write_mask) or (write_old and (not write_mask));
-        end function;
+    FUNCTION update_bits(
+        write_in : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+        write_mask : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0);
+        write_old : STD_LOGIC_VECTOR((XLEN - 1) DOWNTO 0))
+        RETURN STD_LOGIC_VECTOR IS
+    BEGIN
+        RETURN (write_in AND write_mask) OR (write_old AND (NOT write_mask));
+    END FUNCTION;
 
-begin
+BEGIN
 
     -- synchronous write
-    process(clock, nRST, int_vec)
-    begin
+    PROCESS (clock, nRST, int_vec)
+    BEGIN
 
         -- Handle reset 
-        if nRST = '0' then
+        IF nRST = '0' THEN
 
-            mstatus     <= X"0000_1800";
-            misa        <= X"4000_0100";
-            mie         <= (others => '0');
-            mtvec       <= (others => '0');
-            mscratch    <= (others => '0');
-            mepc        <= (others => '0');
-            mcause      <= (others => '0');
-            mtval       <= (others => '0');
-            mip         <= (others => '0');
+            mstatus <= X"0000_1800";
+            misa <= X"4000_0100";
+            mie <= (OTHERS => '0');
+            mtvec <= (OTHERS => '0');
+            mscratch <= (OTHERS => '0');
+            mepc <= (OTHERS => '0');
+            mcause <= (OTHERS => '0');
+            mtval <= (OTHERS => '0');
+            mip <= (OTHERS => '0');
 
-        -- Handle standard writes
-        elsif rising_edge(clock) and (clock_en = '1') then
+            -- Handle standard writes
+        ELSIF rising_edge(clock) AND (clock_en = '1') THEN
 
             -- Synchronizing the MIP register
-            mip   <=  int_vec and mie; 
+            mip <= int_vec AND mie;
 
-            if (we = '1') then
+            IF (we = '1') THEN
 
-                case wa is
-                    when r_MSTATUS =>
-                        mstatus     <= update_bits(wd, MSTATUS_W_MASK,  mstatus);
-                    -- when r_MISA =>
-                    --     misa        <= update_bits(wd, MISA_W_MASK,     misa); -- Mask is 0
-                    when r_MIE =>
-                        mie         <= update_bits(wd, MIE_W_MASK,      mie);
-                    when r_MTVEC =>
-                        mtvec       <= update_bits(wd, MTVEC_W_MASK,    mtvec);
-                    when r_MSCRATCH =>
-                    --    mscratch    <= update_bits(wd, MSCRATCH_W_MASK, mscratch);
-                        mscratch    <= wd;                                        -- Mask is FFFF_FFFF, so direct copy           
-                    when r_MEPC =>
-                        mepc        <= update_bits(wd, MEPC_W_MASK,     mepc);
-                    when r_MCAUSE =>
-                        mcause      <= update_bits(wd, MCAUSE_W_MASK,   mcause);
-                    -- when r_MTVAL =>
-                    --     mtval       <= update_bits(wd, MTVAL_W_MASK,    mtval); -- Mask is 0
-                    -- when r_MIP    =>
-                    --     mip         <= update_bits(wd, MIP_W_MASK,   mcause);  -- Mask is 0
-                    when others =>
+                CASE wa IS
+                    WHEN r_MSTATUS =>
+                        mstatus <= update_bits(wd, MSTATUS_W_MASK, mstatus);
+                        -- when r_MISA =>
+                        --     misa        <= update_bits(wd, MISA_W_MASK,     misa); -- Mask is 0
+                    WHEN r_MIE =>
+                        mie <= update_bits(wd, MIE_W_MASK, mie);
+                    WHEN r_MTVEC =>
+                        mtvec <= update_bits(wd, MTVEC_W_MASK, mtvec);
+                    WHEN r_MSCRATCH =>
+                        --    mscratch    <= update_bits(wd, MSCRATCH_W_MASK, mscratch);
+                        mscratch <= wd; -- Mask is FFFF_FFFF, so direct copy           
+                    WHEN r_MEPC =>
+                        mepc <= update_bits(wd, MEPC_W_MASK, mepc);
+                    WHEN r_MCAUSE =>
+                        mcause <= update_bits(wd, MCAUSE_W_MASK, mcause);
+                        -- when r_MTVAL =>
+                        --     mtval       <= update_bits(wd, MTVAL_W_MASK,    mtval); -- Mask is 0
+                        -- when r_MIP    =>
+                        --     mip         <= update_bits(wd, MIP_W_MASK,   mcause);  -- Mask is 0
+                    WHEN OTHERS =>
                         -- do nothing
-                end case;
-                     
-            
-            end if;
-                
-        end if;
+                END CASE;
+            END IF;
 
-    end process;
-    
+        END IF;
+
+    END PROCESS;
+
     -- asynchronous reads (combinational muxes)
-    rd1 <=      mstatus     when (ra1 = r_MSTATUS)  else
-                misa        when (ra1 = r_MISA)     else
-                mie         when (ra1 = r_MIE)      else
-                mtvec       when (ra1 = r_MTVEC)    else
-                mscratch    when (ra1 = r_MSCRATCH) else
-                mepc        when (ra1 = r_MEPC)     else
-                mcause      when (ra1 = r_MCAUSE)   else
-                mtval       when (ra1 = r_MTVAL)    else
-                mip         when (ra1 = r_MIP)      else
-                (others => '0');
+    rd1 <= mstatus WHEN (ra1 = r_MSTATUS) ELSE
+        misa WHEN (ra1 = r_MISA) ELSE
+        mie WHEN (ra1 = r_MIE) ELSE
+        mtvec WHEN (ra1 = r_MTVEC) ELSE
+        mscratch WHEN (ra1 = r_MSCRATCH) ELSE
+        mepc WHEN (ra1 = r_MEPC) ELSE
+        mcause WHEN (ra1 = r_MCAUSE) ELSE
+        mtval WHEN (ra1 = r_MTVAL) ELSE
+        mip WHEN (ra1 = r_MIP) ELSE
+        (OTHERS => '0');
 
     -- always read the MIP port.
-    int_out <=  mip(11);
-    int_en  <=  mstatus(3);
+    int_out <= mip(11);
+    int_en <= mstatus(3);
 
-end architecture;
+END ARCHITECTURE;
--- src/core/csr/csr_registers_tb.vhd
+++ src/core/csr/csr_registers_tb.vhd
@@ -1,124 +1,124 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.common.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.common.ALL;
 
-entity csr_registers_tb is
-end entity;
+ENTITY csr_registers_tb IS
+END ENTITY;
 
-architecture behavioral of csr_registers_tb is
+ARCHITECTURE behavioral OF csr_registers_tb IS
 
-        signal clock_t :    std_logic                       := '0';
-        signal clock_en_t : std_logic                       := '0';
-        signal nRST_t :     std_logic                       := '0';
-        signal we_t :       std_logic                       := '0';
-        signal wa_t :       csr_register                    := r_MSTATUS;
-        signal wd_t :       std_logic_vector(31 downto 0)   := (others => '1');
-        signal ra1_t :      csr_register                    := r_MSTATUS;
-        signal rd1_t :      std_logic_vector(31 downto 0)   := (others => '0');
-        signal int_vec_t :  std_logic_vector(31 downto 0)   := (others => '0');
-        signal int_out_t :  std_logic                       := '0';
+    SIGNAL clock_t : STD_LOGIC := '0';
+    SIGNAL clock_en_t : STD_LOGIC := '0';
+    SIGNAL nRST_t : STD_LOGIC := '0';
+    SIGNAL we_t : STD_LOGIC := '0';
+    SIGNAL wa_t : csr_register := r_MSTATUS;
+    SIGNAL wd_t : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '1');
+    SIGNAL ra1_t : csr_register := r_MSTATUS;
+    SIGNAL rd1_t : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL int_vec_t : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL int_out_t : STD_LOGIC := '0';
 
-    begin
+BEGIN
 
-        U1 : entity work.clock(behavioral)
-            port map (
-                clk         => clock_t,
-                nRST        => nRST_t,
-                clk_en      => clock_en_t
-            );
+    U1 : ENTITY work.clock(behavioral)
+        PORT MAP(
+            clk => clock_t,
+            nRST => nRST_t,
+            clk_en => clock_en_t
+        );
 
-        U2 : entity work.csr_registers(rtl)
-            port map (
-                clock       => clock_t,
-                clock_en    => clock_en_t,
-                nRST        => nRST_t,
-                we          => we_t,
-                wa          => wa_t,
-                wd          => wd_t,
-                ra1         => ra1_t,
-                rd1         => rd1_t,
-                int_vec     => int_vec_t,
-                int_out     => int_out_t
-            );
+    U2 : ENTITY work.csr_registers(rtl)
+        PORT MAP(
+            clock => clock_t,
+            clock_en => clock_en_t,
+            nRST => nRST_t,
+            we => we_t,
+            wa => wa_t,
+            wd => wd_t,
+            ra1 => ra1_t,
+            rd1 => rd1_t,
+            int_vec => int_vec_t,
+            int_out => int_out_t
+        );
 
-        -- clock
-        P1 : process
-        begin
-            wait for 5 ns;
-            clock_t <= not clock_t;
-        end process;
+    -- clock
+    P1 : PROCESS
+    BEGIN
+        WAIT FOR 5 ns;
+        clock_t <= NOT clock_t;
+    END PROCESS;
 
-        -- nRST
-        P2 : process
-        begin
-            nRST_t  <= '0';
-            wait for 15 ns;
-            nRST_t  <= '1';
-            wait;
-        end process;
+    -- nRST
+    P2 : PROCESS
+    BEGIN
+        nRST_t <= '0';
+        WAIT FOR 15 ns;
+        nRST_t <= '1';
+        WAIT;
+    END PROCESS;
 
-        -- Write data
-        P3 : process
-        begin
-            wait for 15 ns;
-            wait for 200 ns;
-            wa_t    <= r_MSTATUS;
-            we_t    <= '1';
-            wait for 20 ns;
-            wa_t    <= r_MISA;
-            wait for 20 ns;
-            wa_t    <= r_MIE;
-            wait for 20 ns;
-            wa_t    <= r_MTVEC;
-            wait for 20 ns;
-            wa_t    <= r_MSCRATCH;
-            wait for 20 ns;
-            wa_t    <= r_MEPC;
-            wait for 20 ns;
-            wa_t    <= r_MCAUSE;
-            wait for 20 ns;
-            wa_t    <= r_MTVAL;
-            wait for 20 ns;
-            wa_t    <= r_MIP;
-            wait for 20 ns;
-            we_t    <= '0';
-            wd_t    <= X"0000_0000";
-            wait for 200 ns;
-        end process;
-        
-        -- Read data
-        P4 : process
-        begin
-            wait for 20 ns;
-            ra1_t   <= r_MSTATUS;
-            wait for 20 ns;
-            ra1_t   <= r_MISA;
-            wait for 20 ns;
-            ra1_t   <= r_MIE;
-            wait for 20 ns;
-            ra1_t   <= r_MTVEC;
-            wait for 20 ns;
-            ra1_t   <= r_MSCRATCH;
-            wait for 20 ns;
-            ra1_t   <= r_MEPC;
-            wait for 20 ns;
-            ra1_t   <= r_MCAUSE;
-            wait for 20 ns;
-            ra1_t   <= r_MTVAL;
-            wait for 20 ns;
-            ra1_t   <= r_MIP;
-            wait for 20 ns;
-        end process;
+    -- Write data
+    P3 : PROCESS
+    BEGIN
+        WAIT FOR 15 ns;
+        WAIT FOR 200 ns;
+        wa_t <= r_MSTATUS;
+        we_t <= '1';
+        WAIT FOR 20 ns;
+        wa_t <= r_MISA;
+        WAIT FOR 20 ns;
+        wa_t <= r_MIE;
+        WAIT FOR 20 ns;
+        wa_t <= r_MTVEC;
+        WAIT FOR 20 ns;
+        wa_t <= r_MSCRATCH;
+        WAIT FOR 20 ns;
+        wa_t <= r_MEPC;
+        WAIT FOR 20 ns;
+        wa_t <= r_MCAUSE;
+        WAIT FOR 20 ns;
+        wa_t <= r_MTVAL;
+        WAIT FOR 20 ns;
+        wa_t <= r_MIP;
+        WAIT FOR 20 ns;
+        we_t <= '0';
+        wd_t <= X"0000_0000";
+        WAIT FOR 200 ns;
+    END PROCESS;
 
-        -- Emulate interrupt
-        P5 : process
-        begin
-            wait for 15 ns;
-            wait for 600 ns;
-            wait for 18 ns;
-            int_vec_t <= X"FFFF_FFFF";
-            wait;
-        end process;
+    -- Read data
+    P4 : PROCESS
+    BEGIN
+        WAIT FOR 20 ns;
+        ra1_t <= r_MSTATUS;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MISA;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MIE;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MTVEC;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MSCRATCH;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MEPC;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MCAUSE;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MTVAL;
+        WAIT FOR 20 ns;
+        ra1_t <= r_MIP;
+        WAIT FOR 20 ns;
+    END PROCESS;
 
-    end architecture;
+    -- Emulate interrupt
+    P5 : PROCESS
+    BEGIN
+        WAIT FOR 15 ns;
+        WAIT FOR 600 ns;
+        WAIT FOR 18 ns;
+        int_vec_t <= X"FFFF_FFFF";
+        WAIT;
+    END PROCESS;
+
+END ARCHITECTURE;
