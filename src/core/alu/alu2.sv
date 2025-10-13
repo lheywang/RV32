8ed983ff34781e9062ca8d8102b3c0c0c96db47b
lheywang <leonard.heywang@gmail.com>
2025-10-13 10:14:59 +0200

    Added the standard interface to all alus

---
+++ src/core/alu/alu2.sv
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+import core_config_pkg::XLEN;
+import core_config_pkg::alu_commands_t;
+
+
+module alu2 (
+    // Standard interface
+    input   logic                                           clk,
+    input   logic                                           rst_n,
+
+    // Issuer interface
+    input   logic   [(core_config_pkg::XLEN - 1) : 0]       arg0,
+    input   logic   [(core_config_pkg::XLEN - 1) : 0]       arg1,
+    input   alu_commands_t                                  cmd,
+    input   logic   [(core_config_pkg::REG_ADDR_W - 1) : 0] i_rd,
+    output  logic                                           busy,
+    output  logic                                           i_error,
+
+    // Commiter interface
+    output  logic   [(core_config_pkg::XLEN - 1) : 0]       res,
+    output  logic   [(core_config_pkg::REG_ADDR_W - 1) : 0] o_rd,
+    output  logic                                           valid,
+    output  logic                                           o_error,
+    output  logic                                           req,
+    input   logic                                           clear
+
+    // Additionnal interface (optionnal)
+    // None for this ALU.                  
+);
+
+endmodule
