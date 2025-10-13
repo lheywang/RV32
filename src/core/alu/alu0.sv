dab166eb8a06bf3d7b373fdd86b53c7476d30b7d
lheywang <leonard.heywang@gmail.com>
2025-10-13 11:41:48 +0200

    Finished the ALU0 logic

--- src/core/alu/alu0.sv
+++ src/core/alu/alu0.sv
@@ -37,5 +37,104 @@ module alu0 (
     // Additionnal interface (optionnal)
     // None for this ALU.                  
 );
+    /*
+     *  Storages types
+     */
+    logic   [(core_config_pkg::XLEN) : 0]           tmp_res; // One more bit to handle overflow.
+    logic                                           unknown_instr;
+    logic                                           int_req;
+    logic                                           end_of_op;
+
+    /*
+     *  First, perform calculations (outputs from issuer are synchronous to clock).
+     */
+    always_comb begin
+
+        unique case (cmd)
+
+            core_config_pkg::c_ADD : begin
+                // Calculation
+                tmp_res = {1'b0, arg0} + {1'b0, arg1};
+
+                // Setting flags
+                int_req = 1;
+                unknown_instr = 0;
+            end
+            core_config_pkg::c_SUB : begin
+                // Calculation
+                tmp_res = {1'b0, arg0} - {1'b0, arg1};
+
+                // Setting flags
+                int_req = 1;
+                unknown_instr = 0;
+            end 
+            core_config_pkg::c_AND : begin
+                // Calculation
+                tmp_res = {1'b0, arg0} & {1'b0, arg1};
+
+                // Setting flags
+                int_req = 1;
+                unknown_instr = 0;
+            end
+            core_config_pkg::c_OR : begin
+                // Calculation
+                tmp_res = {1'b0, arg0} | {1'b0, arg1};
+
+                // Setting flags
+                int_req = 1;
+                unknown_instr = 0;
+            end
+            core_config_pkg::c_XOR : begin
+                // Calculation
+                tmp_res = {1'b0, arg0} ^ {1'b0, arg1};
+
+                // Setting flags
+                int_req = 1;
+                unknown_instr = 0;
+            end
+            default : begin
+                // Calculation
+                tmp_res = 0;
+
+                // Setting flags
+                int_req = 0;
+                unknown_instr = 1;
+            end
+        endcase
+    end
+
+    /*
+     *  Second, latching the first stage outputs before outputing them for the
+     *  commiter stage.
+     */
+    always_ff @( posedge clk or negedge rst_n) begin
+
+        if (!rst_n || (clear && end_of_op)) begin
+
+            busy <= 0;
+            res <= 0;
+            i_error <= 0;
+            o_error <= 0;
+            req <= 0;
+            o_rd <= 0;
+            valid <= 0;
+            end_of_op <= 0;
+
+        end
+        else begin
+
+            busy <= int_req;
+            res <= tmp_res[(core_config_pkg::XLEN - 1) : 0];
+            i_error <= unknown_instr;
+            o_error <= tmp_res[(core_config_pkg::XLEN)];
+            req <= 0;
+            o_rd <= i_rd;
+            valid <= 1;
+            end_of_op <= 1;
+
+        end
+    end
+
+
 
 endmodule
