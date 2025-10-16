36028f3b921864c7b887626fcbb6ae3f3e67d3b2
lheywang <leonard.heywang@proton.me>
2025-10-16 16:34:15 +0200

    Changed some definition logic width, to match modules requirements

--- src/packages/core_config_pkg.sv
+++ src/packages/core_config_pkg.sv
@@ -7,52 +7,52 @@ package core_config_pkg;
     // -------------------------------------------------------------------------
     // Clocks and resets
     // -------------------------------------------------------------------------
-    parameter int REF_CLK_FREQ              = 200_000_000;      // 200 MHz
-    parameter int CORE_CLK_FREQ             = 100_000_000;      // 100 MHz
-    parameter int CORE_CLK_DUTY             = 50;               // Duty cycle in percentage
-    parameter int RST_TICK_CNT              = 8;                // Number of reference clock cycles for reset
+    parameter int REF_CLK_FREQ                  = 200_000_000;      // 200 MHz
+    parameter int CORE_CLK_FREQ                 = 100_000_000;      // 100 MHz
+    parameter int CORE_CLK_DUTY                 = 50;               // Duty cycle in percentage
+    parameter int RST_TICK_CNT                  = 8;                // Number of reference clock cycles for reset
 
     // -------------------------------------------------------------------------
     // Data path widths
     // -------------------------------------------------------------------------
-    parameter int XLEN                      = 32;               // Register width (32-bit or 64-bit)
-    parameter int MEM_ADDR_W                = XLEN;             // Address width for memory
-    parameter int MEM_DATA_W                = XLEN;             // Data width
+    parameter int XLEN                          = 32;               // Register width (32-bit or 64-bit)
+    parameter int MEM_ADDR_W                    = XLEN;             // Address width for memory
+    parameter int MEM_DATA_W                    = XLEN;             // Data width
 
     // -------------------------------------------------------------------------
     // Registers configuration
     // -------------------------------------------------------------------------
-    parameter int REG_ADDR_W                = 5;                // Number of bits for register index (32 registers)
-    parameter int REG_COUNT                 = 32;               // Number of general-purpose registers
-    parameter int CSR_ADDR_W                = 12;               // Number of bits for register index (32 registers)
-    parameter int CSR_COUNT                 = 23;               // Number of CSR registers
+    parameter int REG_ADDR_W                    = 5;                // Number of bits for register index (32 registers)
+    parameter int REG_COUNT                     = 32;               // Number of general-purpose registers
+    parameter int CSR_ADDR_W                    = 12;               // Number of bits for register index (32 registers)
+    parameter int CSR_COUNT                     = 23;               // Number of CSR registers
 
     // CSR Addresses
     
     // -------------------------------------------------------------------------
     // Instruction fetch parameters
     // -------------------------------------------------------------------------
-    parameter int IF_LEN                    = 32;               // Instruction length
-    parameter int IF_INC                    = 4;                // Offset between two memory addresses.
-    parameter int IF_MAX_ADDR               = 32'h1000_3FFF;    // Maximal address possible.
-    parameter int IF_BASE_ADDR              = 32'h1000_0000;    // Base address of IF stage.
+    parameter int IF_LEN                        = 32;               // Instruction length
+    parameter int IF_INC                        = 4;                // Offset between two memory addresses.
+    parameter int IF_MAX_ADDR                   = 32'h1000_3FFF;    // Maximal address possible.
+    parameter int IF_BASE_ADDR                  = 32'h1000_0000;    // Base address of IF stage.
 
     // -------------------------------------------------------------------------
     // Decoder internal settings
     // -------------------------------------------------------------------------
     // Define how to split an instruction
-    parameter int OPCODE_MSB                = 6;
-    parameter int OPCODE_LSB                = 0;
-    parameter int RD_MSB                    = 11;
-    parameter int RD_LSB                    = 7;
-    parameter int FUNCT3_MSB                = 14;
-    parameter int FUNCT3_LSB                = 12;
-    parameter int RS1_MSB                   = 19;
-    parameter int RS1_LSB                   = 15;
-    parameter int RS2_MSB                   = 24;
-    parameter int RS2_LSB                   = 20;
-    parameter int FUNCT7_MSB                = 31;
-    parameter int FUNCT7_LSB                = 25;
+    parameter int OPCODE_MSB                    = 6;
+    parameter int OPCODE_LSB                    = 0;
+    parameter int RD_MSB                        = 11;
+    parameter int RD_LSB                        = 7;
+    parameter int FUNCT3_MSB                    = 14;
+    parameter int FUNCT3_LSB                    = 12;
+    parameter int RS1_MSB                       = 19;
+    parameter int RS1_LSB                       = 15;
+    parameter int RS2_MSB                       = 24;
+    parameter int RS2_LSB                       = 20;
+    parameter int FUNCT7_MSB                    = 31;
+    parameter int FUNCT7_LSB                    = 25;
 
     // -------------------------------------------------------------------------
     // CSR internal settings
@@ -89,13 +89,13 @@ package core_config_pkg;
     // -------------------------------------------------------------------------
     // ALUs configuration
     // -------------------------------------------------------------------------
-    parameter int MAX_SHIFT_PER_CYCLE       = 8;              // Max bits shifts per cycles. Optimize for LUT / Speed
+    parameter logic [5:0] MAX_SHIFT_PER_CYCLE   = 8;              // Max bits shifts per cycles. Optimize for LUT / Speed
 
     // -------------------------------------------------------------------------
     // Performance counter configuration
     // -------------------------------------------------------------------------
-    parameter int PERF_CNT_LEN              = 64;               // Performance counters are 64 bits anyway.
-    parameter int PERF_CNT_INC              = 1;                // Increment on each tick
+    parameter int PERF_CNT_LEN                  = 64;               // Performance counters are 64 bits anyway.
+    parameter int PERF_CNT_INC                  = 1;                // Increment on each tick
 
     // -------------------------------------------------------------------------
     // Enums
@@ -140,7 +140,7 @@ package core_config_pkg;
         DEC_U, DEC_I, DEC_R, DEC_B, DEC_S, DEC_J, DEC_NONE
     } decoders_t;
 
-    typedef enum logic [5:0] {
+    typedef enum logic [4:0] {
         r_MSTATUS, r_MIE, r_MTVEC, r_MSCRATCH, r_MEPC, r_MCAUSE,
         r_MTVAL, r_MIP, 
         
