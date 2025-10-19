35d3150dfc4a5cc3024fefe3f17cbd9c321a117f
l.heywang <leonard.heywang@proton.me>
2025-10-19 20:32:04 +0200

    Minor improvements, did not fix issue

--- src/core/alu/operations/booth.sv
+++ src/core/alu/operations/booth.sv
@@ -39,13 +39,14 @@ module booth(
     logic   signed  [((2 * core_config_pkg::XLEN) + 1) : 0] Z_temp;
     logic   signed  [((2 * core_config_pkg::XLEN) + 1) : 0] Z_reg;
     logic   signed  [(core_config_pkg::XLEN) : 0]           Y_ext;
+    logic   signed  [(core_config_pkg::XLEN) : 0]           Y_op;
     logic   signed  [(core_config_pkg::XLEN) : 0]           next_Y_ext;
     state_t                                                 next_state;
     state_t                                                 pres_state;
     logic           [1:0]                                   temp;
     logic           [1:0]                                   next_temp;
-    logic           [($clog2(core_config_pkg::XLEN)-1) : 0] count;
-    logic           [($clog2(core_config_pkg::XLEN)-1) : 0] next_count;
+    logic           [($clog2(core_config_pkg::XLEN)) : 0]   count;
+    logic           [($clog2(core_config_pkg::XLEN)) : 0]   next_count;
     logic                                                   next_valid;
 
     always_ff @ (posedge clk or negedge rst_n) begin
@@ -90,6 +91,7 @@ module booth(
                 if(start) begin
 
                     next_state  = START;
+
                     next_temp   = {X[0],1'b0};
                 
                     /*
@@ -112,7 +114,7 @@ module booth(
 
                     if (Y_signed) begin
 
-                        next_Y_ext = {Y[31], Y};         // Sign extend
+                        next_Y_ext = $signed({$unsigned(Y[31]), $unsigned(Y)});         // Sign extend
 
                     end
                     else begin
@@ -134,23 +136,25 @@ module booth(
 
             START: begin
 
+                Y_op = Y_signed ? $signed(Y_ext) : $unsigned(Y_ext);
+
                 /*
                 * 	Not using default will indcate to quartus to infer muxes rather than equal + selectors.
                 * 	This lead to a gain of frequency of about 35 MHz.
                 */
                 case(temp)
-                    2'b00:  Z_temp = Z_reg;                                             // + 0
-                    2'b10:  Z_temp = {Z_reg[65:32] - $signed(Y_ext),    Z_reg[31:0]};   // - Y
-                    2'b01:  Z_temp = {Z_reg[65:32] + $signed(Y_ext),    Z_reg[31:0]};   // + Y
-                    2'b11:  Z_temp = Z_reg;                                             // + 0
+                    2'b00:  Z_temp = Z_reg;                                   // + 0
+                    2'b10:  Z_temp = {Z_reg[65:32] - Y_op,    Z_reg[31:0]};   // - Y
+                    2'b01:  Z_temp = {Z_reg[65:32] + Y_op,    Z_reg[31:0]};   // + Y
+                    2'b11:  Z_temp = Z_reg;                                   // + 0
                 endcase
                 
-                next_temp       = {X[count+1],X[count]};
-                next_count      = count + 1;
-                next_Z          = $signed(Z_temp) >>> 1;
+                next_temp       = Z_reg[1:0];
+                next_count      = count + 1'b1;
+                next_Z          = Z_temp >>> 1;
 
-                next_valid      = (&count) ? 1'b1 : 1'b0; 
-                next_state      = (&count) ? IDLE : pres_state;
+                next_valid      = (count == 31) ? 1'b1 : 1'b0; 
+                next_state      = (count == 31) ? IDLE : pres_state;
 
                 next_Y_ext = 0;
 
