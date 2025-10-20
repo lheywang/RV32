26be5828b7ef3c5103ef3e048bd4be17de704784
l.heywang <leonard.heywang@proton.me>
2025-10-20 17:35:56 +0200

    Added comments and so

--- src/core/alu/operations/srt.sv
+++ src/core/alu/operations/srt.sv
@@ -25,12 +25,11 @@ module srt(
         ERR         = 2'b11
     } state_t;
 
-    state_t pres_state, next_state;
+    state_t                 pres_state;
+    state_t                 next_state;
 
     /*
      * Registers
-     * Structure: [Remainder (33 bits) | Quotient (32 bits)]
-     * Similar to Booth's [AC | QR] structure
      */
     logic [64:0]            AQ_reg;         // Combined register [Remainder:Quotient]
     logic [64:0]            next_AQ;
@@ -90,10 +89,11 @@ module srt(
     end
 
     /*
-     * Combinational logic
+     * Combinational logic part of the divider, where the magic is done
      */
     always_comb begin
-        // Defaults
+
+        // Defaults assignements
         next_state          = pres_state;
         next_AQ             = AQ_reg;
         next_divisor        = divisor_reg;
@@ -104,27 +104,33 @@ module srt(
         next_divisor_neg    = divisor_neg_reg;
         next_quotient_null  = 1'b0;
         next_remainder_null = 1'b0;
-        final_quotient      = '0;
-        final_remainder     = '0;
+        final_quotient      = 32'b0;
+        final_remainder     = 32'b0;
 
         case (pres_state)
+
             IDLE: begin
-                next_count = 6'd0;
-                next_div_by_zero = 1'b0;
-                next_valid = 1'b0;
+                
+                next_count          = 6'd0;
+                next_div_by_zero    = 1'b0;
+                next_valid          = 1'b0;
 
                 if (start) begin
                    
+                    /*
+                     *  Note : The math prevent from dividing by zero. So, we end up in errors
+                     *  that would happen.
+                     */
                     if (divisor == 32'd0) begin
 
-                        next_div_by_zero = 1'b1;
-                        next_AQ = {dividend, 33'h1FFFFFFFF};
-                        next_state = ERR;
+                        next_div_by_zero    = 1'b1;
+                        next_AQ             = {dividend, 33'h1FFFFFFFF};
+                        next_state          = ERR;
 
                     end
 
                     /* 
-                     *  Note : Using two signess for the division lead to incorrect 
+                     *  Note : Using two different signes for the division lead to incorrect 
                      *  and unpredictable results.
                      *
                      *  XORing the signs ensure us to get an error in theses cases, or the
@@ -132,9 +138,9 @@ module srt(
                      */
                     else if (dividend_signed ^ divisor_signed) begin
 
-                        next_div_by_zero = 1'b0;
-                        next_AQ = {dividend, 33'h1FFFFFFFF};
-                        next_state = ERR;
+                        next_div_by_zero    = 1'b0;
+                        next_AQ             = {dividend, 33'h1FFFFFFFF};
+                        next_state          = ERR;
 
                     end 
                     else begin
@@ -144,50 +150,60 @@ module srt(
                         logic [31:0] abs_dividend;
                         logic [31:0] abs_divisor;
 
-                        sign_mask_dividend = {32{dividend_signed && dividend[31]}};
-                        sign_mask_divisor  = {32{divisor_signed  && divisor[31]}};
+                        sign_mask_dividend  = {32{dividend_signed && dividend[31]}};
+                        sign_mask_divisor   = {32{divisor_signed  && divisor[31]}};
 
                         /*
-                         *  Note : This combinational logic enable to negate a number without needing
-                         *  carry chains. The logic is A XOR MASK + 1, where XOR is used as a wanted inverter.
+                         *  Note : This combinational logic enable to negate a number while being 
+                         *  faster than the default assignements.
+                         *  The logic is A XOR MASK + 1, where XOR is used as a commanded inverter.
                          */
-                        abs_dividend = (dividend ^ sign_mask_dividend) + {31'b0, sign_mask_dividend[0]};
-                        abs_divisor  = (divisor  ^ sign_mask_divisor)  + {31'b0, sign_mask_divisor[0]};
+                        abs_dividend        = (dividend ^ sign_mask_dividend) + {31'b0, sign_mask_dividend[0]};
+                        abs_divisor         = (divisor  ^ sign_mask_divisor)  + {31'b0, sign_mask_divisor[0]};
 
-                        next_dividend_neg = dividend_signed && dividend[31];
-                        next_divisor_neg  = divisor_signed  && divisor[31];
+                        next_dividend_neg   = dividend_signed && dividend[31];
+                        next_divisor_neg    = divisor_signed  && divisor[31];
 
-                        next_AQ       = {33'd0, abs_dividend};
-                        next_divisor  = abs_divisor;
+                        next_AQ             = {33'd0, abs_dividend};
+                        next_divisor        = abs_divisor;
 
-                        next_state = DIVIDE;
+                        next_state          = DIVIDE;
                     end
                 end
             end
 
             DIVIDE: begin
+
                 /* verilator lint_off UNUSEDSIGNAL */
                 logic [64:0] shifted_AQ;
                 /* verilator lint_on UNUSEDSIGNAL */
                 logic [32:0] A_part;        // Remainder part (33 bits)
                 logic [32:0] temp_sub;
                 
-                shifted_AQ = AQ_reg << 1;
-                A_part = shifted_AQ[64:32];
+                // Performing the shift
+                shifted_AQ                  = AQ_reg << 1;
+                A_part                      = shifted_AQ[64:32];
                 
-                temp_sub = A_part - {1'b0, divisor_reg};
+                /*
+                 *  Theses lines, the substraction and the selection are on the critical path.
+                 *  Getting better expression here WILL result in a better FMAX.
+                 */
+                // Checking if we can perform another step
+                temp_sub                    = A_part - {1'b0, divisor_reg};
                 case (temp_sub[32])
-                    1'b1 :  next_AQ = {shifted_AQ[64:1], 1'b0};
-                    1'b0 :  next_AQ = {temp_sub, shifted_AQ[31:1], 1'b1};
+                    1'b1 :  next_AQ         = {shifted_AQ[64:1], 1'b0};
+                    1'b0 :  next_AQ         = {temp_sub, shifted_AQ[31:1], 1'b1};
                 endcase
 
-                next_count = count + 6'd1;
+                // Incrementing count
+                next_count                  = count + 6'd1;
 
-                next_quotient_null = (next_AQ[31:0] == 32'd0) ? 1'b1 : 1'b0; 
-                next_remainder_null = (next_AQ[63:32] == 32'd0) ? 1'b1 : 1'b0; 
+                // Registered assignement of the conditions, get use some better results
+                next_quotient_null          = (next_AQ[31:0] == 32'd0) ? 1'b1 : 1'b0; 
+                next_remainder_null         = (next_AQ[63:32] == 32'd0) ? 1'b1 : 1'b0; 
 
                 if (count == 6'd31) begin
-                    next_state = SIGN_FIX;
+                    next_state              = SIGN_FIX;
                 end
             end
 
@@ -196,31 +212,34 @@ module srt(
                 logic [31:0] sign_mask_q;
                 logic [31:0] sign_mask_r;
 
-                sign_mask_q = {32{(dividend_neg_reg ^ divisor_neg_reg) && !quotient_null}};
-                sign_mask_r = {32{(dividend_neg_reg && !remainder_null)}};
+                sign_mask_q                 = {32{(dividend_neg_reg ^ divisor_neg_reg) && !quotient_null}};
+                sign_mask_r                 = {32{(dividend_neg_reg && !remainder_null)}};
 
                 /*
-                 *  Note : This combinational logic enable to negate a number without needing
-                 *  carry chains. The logic is A XOR MASK + 1, where XOR is used as a wanted inverter.
+                 *  Note : This combinational logic enable to negate a number while being 
+                 *  faster than the default assignements.
+                 *  The logic is A XOR MASK + 1, where XOR is used as a commanded inverter.
                  */	
-                final_quotient = (AQ_reg[31:0] ^ sign_mask_q) + {31'b0, sign_mask_q[0]};
-                final_remainder = (AQ_reg[63:32] ^ sign_mask_r) + {31'b0, sign_mask_r[0]};
-                
-                next_AQ = {AQ_reg[64], final_remainder, final_quotient};
+                final_quotient              = (AQ_reg[31:0] ^ sign_mask_q) + {31'b0, sign_mask_q[0]};
+                final_remainder             = (AQ_reg[63:32] ^ sign_mask_r) + {31'b0, sign_mask_r[0]};
+                next_AQ                     = {AQ_reg[64], final_remainder, final_quotient};
 
-                next_valid = 1'b1;
-                next_state = IDLE;
+                next_valid                  = 1'b1;
+                next_state                  = IDLE;
             end
 
+            /*
+             *  Handling errors by exiting early and setting out the valid bit.
+             */
             ERR: begin
-                next_valid = 1'b1;
-                next_state = IDLE;
+                next_valid                  = 1'b1;
+                next_state                  = IDLE;
             end
         endcase
     end
 
     // Output assignments
-    assign quotient = AQ_reg[31:0];
-    assign remainder = AQ_reg[63:32];
+    assign quotient                         = AQ_reg[31:0];
+    assign remainder                        = AQ_reg[63:32];
 
 endmodule
