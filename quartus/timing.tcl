0469688a2b5e7fb4363a0d49febb61a885adb86a
lheywang <leonard.heywang@proton.me>
2025-10-21 23:12:55 +0200

    Updated the script to generate HTML reports of the fanouts / timings / violations

--- quartus/timing.tcl
+++ quartus/timing.tcl
@@ -1,5 +1,13 @@
+# Creating the timing netlist, and ensuring it's complete
 create_timing_netlist
 read_sdc
 update_timing_netlist
-report_clocks
-report_timing -npath 50
+
+# Set the filter here
+set filter "divider|*"
+
+# Providing reports into an HTML file
+report_clocks -file reports/clocks.html
+report_timing -file reports/timing.html -npath 50
+report_net_timing -file reports/delay.html -nworst_delay 20 $filter
+report_net_timing -file reports/fanout.html -nworst_fanout 20 $filter
