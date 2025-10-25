e0271d7268bf629658eea46f5ac37e7f68388cd6
lheywang <leonard.heywang@proton.me>
2025-10-25 11:32:49 +0200

    Added empty assemblies files for the future

---
+++ rtl/core/assemblies/assembly_alu.sv
@@ -0,0 +1,16 @@
+/*
+ *  File :      rtl/core/assembly/assembly_alu.sv
+ *
+ *  Author :    l.heywang <leonard.heywang@proton.me>
+ *  Date :      25/10.2025
+ *  
+ *  Brief :     This file assemble all of the ALU with the issuer
+ *              and commiter units. This is done to make the global
+ *              core.sv file much more readable.
+ */
+
+module assembly_alu (
+
+);
+
+endmodule
---
+++ rtl/core/assemblies/assembly_csr.sv
@@ -0,0 +1,16 @@
+/*
+ *  File :      rtl/core/assembly/assembly_csr.sv
+ *
+ *  Author :    l.heywang <leonard.heywang@proton.me>
+ *  Date :      25/10.2025
+ *  
+ *  Brief :     This file assemble all of the CSR with it's associated
+ *              counters. This make the global core assembly much
+ *              more readable.
+ */
+
+module assembly_csr (
+
+);
+
+endmodule
