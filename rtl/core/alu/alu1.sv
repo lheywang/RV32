b058123aee7308db913cc74aa3b43050e470302f
lheywang <leonard.heywang@proton.me>
2025-11-03 11:11:18 +0100

    Added the handling of the new instructions into the ALU1

--- rtl/core/alu/alu1.sv
+++ rtl/core/alu/alu1.sv
@@ -16,6 +16,11 @@
 import core_config_pkg::XLEN;
 import core_config_pkg::alu_commands_t;
 
+import core_config_pkg::IF_INC;
+import core_config_pkg::IF_MRET_UCODE;
+import core_config_pkg::IF_ECALL_UCODE;
+import core_config_pkg::IF_EBREAK_UCODE;
+
 module alu1 (
     input  logic                                                  clk,
     input  logic                                                  rst_n,
@@ -29,6 +34,7 @@ module alu1 (
     output logic                                                  i_error,
     input  logic                                                  predict_in,
     output logic          [      (core_config_pkg::XLEN - 1) : 0] res,
+    output logic          [      (core_config_pkg::XLEN - 1) : 0] jmp,
     output logic          [(core_config_pkg::REG_ADDR_W - 1) : 0] o_rd,
     output logic                                                  valid,
     output logic                                                  o_error,
@@ -41,11 +47,21 @@ module alu1 (
      *  Storages types
      */
     logic [(core_config_pkg::XLEN) : 0] tmp_res;  // One more bit to handle overflow.
+    logic [(core_config_pkg::XLEN) : 0] tmp_jmp;  // One more bit to handle overflow.
     logic                               unknown_instr;
     logic                               int_req;
     logic                               act_needed;
     logic                               end_of_op;
     logic                               predict_value;
+
+    /*
+     *  Note : Two outputs are used here : jmp and res.
+     *  
+     *  Res :   Used to output the value that WILL be written into the register file
+     *  Jmp :   Used to output the value to be written into the PC.
+     *
+     *  This enable the ability to perform single cycle JAL / JALR instructions and so on.
+     */
 
 
     /*
@@ -61,6 +77,7 @@ module alu1 (
             core_config_pkg::c_SLT: begin
                 // Calculation
                 tmp_res       = ($signed(arg0) < $signed(arg1)) ? 33'b1 : 33'b0;
+                tmp_jmp       = '0;
 
                 // Setting flags
                 act_needed    = 1'b0;
@@ -71,6 +88,7 @@ module alu1 (
             core_config_pkg::c_SLTU: begin
                 // Calculation
                 tmp_res       = ($unsigned(arg0) < $unsigned(arg1)) ? 33'b1 : 33'b0;
+                tmp_jmp       = '0;
 
                 // Setting flags
                 act_needed    = 1'b0;
@@ -84,7 +102,8 @@ module alu1 (
              */
             core_config_pkg::c_BEQ: begin
                 // Calculation
-                tmp_res       = {1'b0, addr} + {1'b0, imm};
+                tmp_jmp       = {1'b0, addr} + {1'b0, imm};
+                tmp_res       = '0;
 
                 // Setting flags
                 act_needed    = (arg0 == arg1) ? 1'b1 : 1'b0;
@@ -94,7 +113,8 @@ module alu1 (
             end
             core_config_pkg::c_BNE: begin
                 // Calculation
-                tmp_res       = {1'b0, addr} + {1'b0, imm};
+                tmp_jmp       = {1'b0, addr} + {1'b0, imm};
+                tmp_res       = '0;
 
                 // Setting flags
                 act_needed    = (arg0 == arg1) ? 1'b0 : 1'b1;
@@ -104,7 +124,8 @@ module alu1 (
             end
             core_config_pkg::c_BGE: begin
                 // Calculation
-                tmp_res       = {1'b0, addr} + {1'b0, imm};
+                tmp_jmp       = {1'b0, addr} + {1'b0, imm};
+                tmp_res       = '0;
 
                 // Setting flags
                 act_needed    = ($signed(arg0) >= $signed(arg1)) ? 1'b1 : 1'b0;
@@ -114,7 +135,8 @@ module alu1 (
             end
             core_config_pkg::c_BLT: begin
                 // Calculation
-                tmp_res       = {1'b0, addr} + {1'b0, imm};
+                tmp_jmp       = {1'b0, addr} + {1'b0, imm};
+                tmp_res       = '0;
 
                 // Setting flags
                 act_needed    = ($signed(arg0) < $signed(arg1)) ? 1'b1 : 1'b0;
@@ -124,7 +146,8 @@ module alu1 (
             end
             core_config_pkg::c_BGEU: begin
                 // Calculation
-                tmp_res       = {1'b0, addr} + {1'b0, imm};
+                tmp_jmp       = {1'b0, addr} + {1'b0, imm};
+                tmp_res       = '0;
 
                 // Setting flags
                 act_needed    = ($unsigned(arg0) >= $unsigned(arg1)) ? 1'b1 : 1'b0;
@@ -134,7 +157,8 @@ module alu1 (
             end
             core_config_pkg::c_BLTU: begin
                 // Calculation
-                tmp_res       = {1'b0, addr} + {1'b0, imm};
+                tmp_jmp       = {1'b0, addr} + {1'b0, imm};
+                tmp_res       = '0;
 
                 // Setting flags
                 act_needed    = ($unsigned(arg0) < $unsigned(arg1)) ? 1'b1 : 1'b0;
@@ -142,16 +166,83 @@ module alu1 (
                 int_req       = 1'b1;
                 unknown_instr = 1'b0;
             end
+
+            /*
+             *  Jumps
+             */
+            core_config_pkg::c_JAL: begin
+                // Calculation
+                tmp_jmp       = {1'b0, addr} + IF_INC;
+                tmp_res       = '0;
+
+                // Setting flags
+                act_needed    = 1'b0;  // always predicted
+                predict_value = 1'b0;
+                int_req       = 1'b0;
+                unknown_instr = 1'b0;
+            end
+            core_config_pkg::c_JALR: begin
+                // Calculation
+                tmp_jmp       = {1'b0, addr} + {1'b0, arg0} & ~1;
+                tmp_res       = {1'b0, addr} + IF_INC;
+
+                // Setting flags
+                act_needed    = 1'b1;  // always jumped
+                predict_value = 1'b0;
+                int_req       = 1'b1;
+                unknown_instr = 1'b0;
+            end
+            /*
+             *  Special instructions
+             */
+            core_config_pkg::c_EBREAK: begin
+                // Calculation
+                tmp_jmp       = {1'b0, IF_EBREAK_UCODE};
+                tmp_res       = '0;
+
+                // Setting flags
+                act_needed    = 1'b1;  // always jumped
+                predict_value = 1'b0;
+                int_req       = 1'b1;
+                unknown_instr = 1'b0;
+            end
+            core_config_pkg::c_ECALL: begin
+                // Calculation
+                tmp_jmp       = {1'b0, IF_ECALL_UCODE};
+                tmp_res       = '0;
+
+                // Setting flags
+                act_needed    = 1'b1;  // always jumped
+                predict_value = 1'b0;
+                int_req       = 1'b1;
+                unknown_instr = 1'b0;
+            end
+            core_config_pkg::c_MRET: begin
+                // Calculation
+                tmp_jmp       = {1'b0, IF_MRET_UCODE};
+                tmp_res       = '0;
+
+                // Setting flags
+                act_needed    = 1'b1;  // always jumped
+                predict_value = 1'b0;
+                int_req       = 1'b1;
+                unknown_instr = 1'b0;
+            end
+            /*
+             *  Default case
+             */
             default: begin
                 // Calculation
                 tmp_res       = 33'b0;
+                tmp_jmp       = 33'b0;
 
                 // Setting flags
-                act_needed    = 1'b0;
+                act_needed    = 1'b1;  // always jumped
                 predict_value = 1'b0;
-                int_req       = 1'b0;
-                unknown_instr = 1'b1;
+                int_req       = 1'b1;
+                unknown_instr = 1'b0;
             end
+
         endcase
     end
 
@@ -165,6 +256,7 @@ module alu1 (
 
             busy      <= 1'b0;
             res       <= 32'b0;
+            jmp       <= 32'b0;
             i_error   <= 1'b0;
             o_error   <= 1'b0;
             req       <= 1'b0;
@@ -176,6 +268,7 @@ module alu1 (
 
             busy       <= 1'b0;
             res        <= 32'b0;
+            jmp        <= 32'b0;
             i_error    <= 1'b0;
             o_error    <= 1'b0;
             req        <= 1'b0;
@@ -189,8 +282,9 @@ module alu1 (
 
             busy       <= int_req;
             res        <= tmp_res[(core_config_pkg::XLEN-1) : 0];
+            jmp        <= tmp_jmp[(core_config_pkg::XLEN-1) : 0];
             i_error    <= unknown_instr;
-            o_error    <= tmp_res[(core_config_pkg::XLEN)];
+            o_error    <= tmp_res[(core_config_pkg::XLEN)] | tmp_jmp[(core_config_pkg::XLEN)];
             req        <= act_needed;
             o_rd       <= i_rd;
             valid      <= 1'b1;
