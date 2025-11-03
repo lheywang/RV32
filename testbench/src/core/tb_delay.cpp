1a221d0ee307cc2ef741dc616800d0a6b6e24b4c
lheywang <leonard.heywang@proton.me>
2025-11-03 21:04:25 +0100

    Created the delay testbench

--- testbench/src/core/tb_delay.cpp
+++ testbench/src/core/tb_delay.cpp
@@ -12,11 +12,9 @@ int main(int argc, char **argv)
     tb.dut->clk_en = 1;
     tb.reset();
 
-    unsigned int data[] = {
-        0x11111111, 0x22222222, 0x33333333, 0x44444444, 0x55555555, 0x66666666, 0x77777777,
-        0x88888888, 0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC, 0xDDDDDDDD, 0xEEEEEEEE,
-        0xFFFFFFFF
-    };
+    unsigned int data[] = {0x11111111, 0x22222222, 0x33333333, 0x44444444, 0x55555555,
+                           0x66666666, 0x77777777, 0x88888888, 0x99999999, 0xAAAAAAAA,
+                           0xBBBBBBBB, 0xCCCCCCCC, 0xDDDDDDDD, 0xEEEEEEEE, 0xFFFFFFFF};
 
     tb.dut->din = data[0];
     tb.tick();
